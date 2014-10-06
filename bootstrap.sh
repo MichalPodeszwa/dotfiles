0d6ed341c04f2dd57039a1dcd4624ff620d76b4c
Michał Podeszwa <michal.podeszwa@gmail.com>
2014-10-06 12:22:12 +0200

    Bootstrap now can change your default shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,5 +10,11 @@ ln -s $PWD/.zshrc ~/.zshrc
 ln -s $PWD/terminator ~/.config/terminator
 ln -s $PWD/sublime/User ~/.config/sublime-text-3/Packages/User
 ln -s $PWD/sublime/Installed\ Packages ~/.config/sublime-text-3/Installed\ Packages
-
 echo "DONE"
+
+read -p "Do you want to change your shell to zsh? (y/n)" choice
+case "$choice" in
+  y|Y ) chsh -s /usr/bin/zsh;;
+  n|N ) exit 0;;
+  * ) echo "invalid";;
+esac
