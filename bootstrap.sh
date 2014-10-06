5b2b2b30bbd14e9b33b9f00ea0f92b6f40f1b706
Michał Podeszwa <michal.podeszwa@gmail.com>
2014-10-06 20:24:09 +0200

    Fixed bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.config/terminator ~/.config/sublime-text-3/Packages
 ln -s $PWD/.inputrc ~/.inputrc
 ln -s $PWD/.pip ~/.pip
 ln -s $PWD/.zshrc ~/.zshrc
-ln -s $PWD/terminator ~/.config/terminator
+ln -s $PWD/terminator ~/.config/
 ln -s $PWD/sublime/User ~/.config/sublime-text-3/Packages/User
 ln -s $PWD/sublime/Installed\ Packages ~/.config/sublime-text-3/Installed\ Packages
 echo "DONE"
